6af9fb63cfb03947ebc738f6803c40bb6479b469
Swaroop Sridhar <swaroop.sridhar@microsoft.com>
2020-04-30 19:09:59 -0700

    Fix Generate Bundle

--- src/Tasks/Microsoft.NET.Build.Tasks/GenerateBundle.cs
+++ src/Tasks/Microsoft.NET.Build.Tasks/GenerateBundle.cs
@@ -5,8 +5,6 @@ using Microsoft.Build.Framework;
 using Microsoft.NET.HostModel.Bundle;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
 
 namespace Microsoft.NET.Build.Tasks
 {
@@ -23,21 +21,12 @@ namespace Microsoft.NET.Build.Tasks
         [Required]
         public string TargetFrameworkVersion { get; set; }
         [Required]
-        public string RuntimeIdentifier { get; set; }
-
-        [Required]
         public bool ShowDiagnosticOutput { get; set; }
-
-        [Output]
-        public ITaskItem[] ExcludedFiles { get; set; }
 
         protected override void ExecuteCore()
         {
-            OSPlatform targetOS = RuntimeIdentifier.StartsWith("win") ? OSPlatform.Windows :
-                                  RuntimeIdentifier.StartsWith("osx") ? OSPlatform.OSX : OSPlatform.Linux;
-
             BundleOptions options = BundleOptions.BundleAllContent | (IncludeSymbols ? BundleOptions.BundleSymbolFiles : BundleOptions.None);
-            var bundler = new Bundler(AppHostName, OutputDir, options, targetOS, new Version(TargetFrameworkVersion), ShowDiagnosticOutput);
+            var bundler = new Bundler(AppHostName, OutputDir, options, targetFrameworkVersion: new Version(TargetFrameworkVersion), diagnosticOutput: ShowDiagnosticOutput);
             var fileSpec = new List<FileSpec>(FilesToBundle.Length);
 
             foreach (var item in FilesToBundle)
@@ -47,12 +36,6 @@ namespace Microsoft.NET.Build.Tasks
             }
 
             bundler.GenerateBundle(fileSpec);
-			
-			// Certain files are excluded from the bundle, based on BundleOptions.
-			// For example, native files and contents files are excluded by default.
-			// Return the set of excluded files in ExcludedFiles, so that they can be placed in the publish directory.
-			
-            ExcludedFiles = FilesToBundle.Zip(fileSpec, (item, spec) => (spec.Excluded) ? item : null).Where(x => x != null).ToArray();
         }
     }
 }
